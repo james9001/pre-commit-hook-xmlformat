5ffcd8477effdd095da3ec0346c80239dc9614f3
james9001 <james9001@james9001.net>
2022-07-15 20:50:29 +1000

    Fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[],
 
     scripts=[
-        'pre_commit_hooks/xml_format.py',
+        'pre_commit_hooks/xml_format',
     ],
 
     # explicitly declare packages so setuptools does not attempt auto discovery
